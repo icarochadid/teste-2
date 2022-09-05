d55081d9afd6f98d9eb67307d120a781e08be67c
icarochadid <icaroeduardoeduardo@gmail.com>
2022-09-05 05:22:34 -0300

    .

---
+++ style.js
@@ -0,0 +1,6 @@
+function revelar() {
+    document.getElementById('img').src= "puta.jpg";
+}
+function voltar() {
+    document.getElementById('img').src= "WhatsApp Image 2022-09-03 at 16.30.40.jpeg";
+}
